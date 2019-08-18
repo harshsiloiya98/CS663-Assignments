785d7c14c68476516abd36f8a5e6ed95c825b7c8
Harsh Siloiya <harshsiloiya@gmail.com>
2019-08-19 02:58:28 +0530

    Added comments

--- assignment-1-transformations/1/codes/myShrinkImageByFactorD.m
+++ assignment-1-transformations/1/codes/myShrinkImageByFactorD.m
@@ -1,7 +1,11 @@
 function [] = myShrinkImageByFactorD(img, d)
 % Shrinks image by a factor 'd' along each dimension
+
 [r, c] = size(img);
+
+% Samples only the d-th rows and columns from the image
 newImg = img(d:d:r, d:d:c);
+
 imshow(newImg);
 axis image;
 axis on;
