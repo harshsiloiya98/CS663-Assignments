4bf365e8fdb4f4eff0d26853c53760e99568f5fb
Bhavesh Dhingra <bhavesh4898@gmail.com>
2019-08-26 19:10:21 +0530

    A2 - Q3 code

--- assignment-2-filtering/3/code/myMainScript.m
+++ assignment-2-filtering/3/code/myMainScript.m
@@ -2,5 +2,109 @@
 
 tic;
 %% Your code here
+clear;
+clc;
+
+
+%% Barbara
+image1 = load('../data/barbara.mat');
+image1 = mat2gray(image1.imageOrig);
+
+% Scale
+image1 = imgaussfilt(image1,0.66);
+image1 = image1(1:2:end,1:2:end);
+
+% Corrupt
+corruptImage1 = image1 + randn(size(image1)) .* 0.05;
+
+% Filter
+filteredImage1 = myPatchBasedFiltering(corruptImage1, 25, 9, 0.5);
+
+% Display
+figure('Name', 'Patch-Based Filtering on Image 1');
+subplot(1,3,1), imshow(image1), colorbar, title('Original Image');
+subplot(1,3,2), imshow(corruptImage1), colorbar, title('Corrupted Image');
+subplot(1,3,3), imshow(filteredImage1), colorbar, title('Filtered Image');
+
+%RSMD
+rsmd1 = myRSMD(filteredImage1, image1);
+
+
+
+%% Grass
+
+image2 = imread('../data/grass.png');
+image2 = mat2gray(image2);
+
+% Scale
+% image2 = imgaussfilt(image2,0.66);
+% image2 = image2(1:2:end,1:2:end);
+
+% Corrupt
+corruptImage2 = image2 + randn(size(image2)) .* 0.05;
+
+% Filter
+filteredImage2 = myPatchBasedFiltering(corruptImage2, 25, 9, 0.5);
+
+% Display
+figure('Name', 'Patch-Based Filtering on Image 2');
+subplot(1,3,1), imshow(image2), colorbar, title('Original Image');
+subplot(1,3,2), imshow(corruptImage2), colorbar, title('Corrupted Image');
+subplot(1,3,3), imshow(filteredImage2), colorbar, title('Filtered Image');
+
+%RSMD
+rsmd2 = myRSMD(filteredImage2, image2);
+    
+
+%% HoneyComb
+
+image3 = imread('../data/honeyCombReal.png');
+image3 = mat2gray(image3);
+
+% Scale
+% image3 = imgaussfilt(image3,0.66);
+% image3 = image3(1:2:end,1:2:end);
+
+% Corrupt
+corruptImage3 = image3 + randn(size(image3)) .* 0.05;
+
+% Filter
+filteredImage3 = myPatchBasedFiltering(corruptImage3, 25, 9, 0.5);
+
+% Display
+figure('Name', 'Patch-Based Filtering on Image 3');
+subplot(1,3,1), imshow(image3), colorbar, title('Original Image');
+subplot(1,3,2), imshow(corruptImage3), colorbar, title('Corrupted Image');
+subplot(1,3,3), imshow(filteredImage3), colorbar, title('Filtered Image');
+
+%RSMD
+rsmd3 = myRSMD(filteredImage3, image3);
+
+ 
+%% Display mask used to make patches isotropic
+
+mask = ones(25,25);
+rCenter = 13;
+cCenter = 13;
+for r=1:25
+    for c=1:25
+        mask(r,c) = mask(r,c) * exp(-((rCenter-r)^2+(cCenter-c)^2)/100);
+    end
+end
+figure('Name', 'Mask');
+imshow(mask);
+
+
+
+%%
 
 toc;
+
+function rsmd = myRSMD(image1, image2)
+    rsmd = image1(:) - image2(:);
+    rsmd = rsmd .^ 2;
+    rsmd = sum(rsmd);
+    rsmd = rsmd / size(image1(:),1);
+    rsmd = sqrt(rsmd);
+end
+
---
+++ assignment-2-filtering/3/code/myPatchBasedFiltering.m
@@ -0,0 +1,64 @@
+function filteredImage = myPatchBasedFiltering(corruptImage, windowSize, patchSize, sigma)
+% Function for patch based filtering
+
+    filteredImage = zeros(size(corruptImage));
+    [R,C] = size(corruptImage);
+    halfWindowSize = floor(windowSize/2);
+    halfPatchSize = floor(patchSize/2);
+    
+    for r=1:R
+        disp(r);
+        % Find top and bottom of window and patch around current pixel
+        patchTop = max(1, r-halfPatchSize);
+        patchBottom = min(R, r+halfPatchSize);
+        windowTop = max(1, r-halfWindowSize);    
+        windowBottom = min(R, r+halfWindowSize);   
+        
+        for c=1:C
+            % Find left and right of window and patch around current pixel
+            patchLeft = max(1, c-halfPatchSize);
+            patchRight = min(C, c+halfPatchSize);
+            windowLeft = max(1, c-halfWindowSize);
+            windowRight = min(C, c+halfWindowSize);
+            
+            window = corruptImage(windowTop:windowBottom,windowLeft:windowRight);
+            weights = zeros(size(window));
+            
+            % Loop around the window
+            for r1=windowTop:windowBottom
+                % Calculate top and bottom of patch around other pixel
+                patch1Top = max(1, r1-halfPatchSize);
+                patch1Bottom = min(R, r1+halfPatchSize);
+                
+                for c1=windowLeft:windowRight
+                    % Calculate left and right of patch around other pixel
+                    patch1Left = max(1, c1-halfPatchSize);
+                    patch1Right = min(C, c1+halfPatchSize);
+                    
+                    % Choose the size acc to shorter of the patches
+                    dt = min(r-patchTop, r1-patch1Top);
+                    db = min(patchBottom-r, patch1Bottom-r1);
+                    dl = min(c-patchLeft, c1-patch1Left);
+                    dr = min(patchRight-c, patch1Right-c1);
+                    
+                    % Compute the patches
+                    patch = corruptImage(r-dt:r+db, c-dl:c+dr);
+                    patch1 = corruptImage(r1-dt:r1+db, c1-dl:c1+dr);
+                    
+                    % Calculate weight using distance between patches
+                    w = exp(-sum(sum((patch-patch1).^2))/sigma^2);
+                    % Use mask to make patch isoropic
+                    w = w * exp(-((r1-r)^2+(c1-c)^2)/100);
+                    % Assign the weight in matrix
+                    weights(r1-windowTop+1, c1-windowLeft+1) = w;                    
+                end
+            end
+            
+            % Normalise the weight matrix
+            weights = weights / sum(sum(weights));
+            
+            filteredImage(r,c) = sum(sum(weights .* window));
+         end
+    end
+     
+end
