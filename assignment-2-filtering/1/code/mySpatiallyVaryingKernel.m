483c324150cd62fef2953307d3f8aa0aa2b0c81e
atharvn <32217555+atharvn@users.noreply.github.com>
2019-09-02 23:39:25 +0530

    Updated

--- assignment-2-filtering/1/code/mySpatiallyVaryingKernel.m
+++ assignment-2-filtering/1/code/mySpatiallyVaryingKernel.m
@@ -1,32 +1,51 @@
-function [ outputImg ] = mySpatiallyVaryingKernel(originalImg, inputImg, threshold)
+function [ outputImg, radius_matrix ] = mySpatiallyVaryingKernel(originalImg, inputImg, threshold)
 % originalImg is the given image (bird.jpg)
 % inputImg = black and white image seprating foreground and background
 
 % figure('Name', ' original image'); imshow(uint8(originalImg));
-figure('Name', ' edged image'); imshow(uint8(inputImg));
+% figure('Name', ' edged image'); imshow(uint8(inputImg));
 radius_matrix = bwdist(inputImg,'euclidean'); %calculates distance of each pixel from nearest non-zero pixel value
 % figure('Name', ' radial distance image'); imshow(uint8(radius_matrix));
 
 radius_matrix(radius_matrix>threshold) = [threshold];
-figure('Name', ' thresholded radial distance image'); imshow(uint8(radius_matrix));
+% figure('Name', ' thresholded radial distance image'); imshow(uint8(radius_matrix));
 
 [a,b,c] = size(originalImg);
-for i=1:a
-    for j=1:b
-        for k=1:c
-            r = radius_matrix(i,j); %r = d_p or d_thresh
-            x=1:(2*r+1);
-            y=1:(2*r+1);
-            [X,Y] = meshgrid(x,y);
-            X(( (X-(r+1)).^2 + (Y-(r+1)).^2 ) > 1)=0;
-            X(( (X-(r+1)).^2 + (Y-(r+1)).^2 ) < 1)=1;
-            X = X/sum(X);
-%             print(class(originalImg));
-%             print(class(X));
-            outputImg = conv2(originalImg(:,:,k), X, "same");
-        end
+% for k=1:c
+%     for i=1:a
+%         for j=1:b
+%             r = radius_matrix(i,j); %r = d_p or d_thresh
+% %             x=1:(2*r+1);
+% %             y=1:(2*r+1);
+% %             [X,Y] = meshgrid(x,y);
+% %             X(( (X-(r+1)).^2 + (Y-(r+1)).^2 ) > r*r)=0;
+% %             X(( (X-(r+1)).^2 + (Y-(r+1)).^2 ) <= r.^2)=1;
+% %             X = X/sum(X);
+%             kernel = fspecial('disk',double(r));
+%             kernel (kernel>0) = 1;
+%             kernel = kernel/sum(kernel(:));
+%             temp = conv2(originalImg(:,:,k), kernel, "same");
+%                         outputImg(i,j,k)=temp(i,j,k);
+%         end
+%     end
+% end
+for r=40:-1:1
+    kernel = fspecial('disk',double(r));
+    kernel (kernel>0) = 1;
+    kernel = kernel/sum(kernel(:));
+    for k=1:3
+        temp(:,:,k) = conv2(originalImg(:,:,k), kernel, 'same');
     end
-end
+    for i=1:a
+        for j=1:b
+            if (r == radius_matrix(i,j))
+                outputImg(i,j,:) = temp(i,j,:);
+            end
+            if (radius_matrix(i,j) == 0)
+                outputImg(i,j,:) = originalImg(i,j,:);
+            end
+        end
+    end          
 end
 % figure('Name', ' original image'); imshow(uint8(originalImg));
 % figure('Name', ' output image'); imshow(uint8(outputImg));
