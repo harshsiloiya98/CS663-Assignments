4c6f4b9ed63db69285d27a4538d60561e30ddd52
Harsh Siloiya <harshsiloiya@gmail.com>
2019-10-13 16:10:06 +0530

    Added code for eig function

--- assignment-4-SVD_FaceRecognition/1/code/getEigenvectors.m
+++ assignment-4-SVD_FaceRecognition/1/code/getEigenvectors.m
@@ -1,6 +1,15 @@
-function [eigenvectors] = getEigenvectors(trainingSet, k)
+function [eigenvectors] = getEigenvectors(X, k, method)
 % calculates the eigenvectors for the training set images using
-% singular-value decomposition
+% singular-value decomposition or finding eigenvectors of X'X
 
-[U, ~, ~] = svd(trainingSet, 'econ');
-eigenvectors = U(:, 1:k);
+if (strcmp(method, 'eig') == 1)
+    L = X' * X;
+    [W, D] = eig(L);
+    V = normalize(X * W, 1);
+    [~, ind] = sort(diag(D), 'descend');
+    V = V(:, ind);
+    eigenvectors = V(:, 1:k);
+elseif (strcmp(method, 'svd') == 1)
+    [U, ~, ~] = svd(X, 'econ');
+    eigenvectors = U(:, 1:k);
+end
--- assignment-4-SVD_FaceRecognition/1/code/myMainScript.m
+++ assignment-4-SVD_FaceRecognition/1/code/myMainScript.m
@@ -2,6 +2,8 @@
 
 tic;
 
+%% NOTE: The dataset folders should be placed according to the paths set in the code
+
 %% Data Preprocessing
 
 numPeople = 32;
@@ -26,7 +28,7 @@ end
 trainingSet = reshape(trainingSet, [r * c, N]);
 testingSet = reshape(testingSet, [r * c, M]);
 
-%% Face recognition algorithm
+%% Face recognition algorithm (dataset used - ORL)
 
 meanImg = mean(trainingSet, 2);
 for i = 1:N
@@ -38,12 +40,13 @@ end
 
 
 K = [1 2 3 5 10 15 20 30 50 75 100 150 170];
-squaredDifferences = zeros(size(K));
 recognitionRates = zeros(size(K));
 
-% calculating the eigenvectors for each k
+% calculating the recognition rate for each k
 for i = 1:size(K, 2)
-    eV = getEigenvectors(trainingSet, K(i));
+    % change the third argument of getEigenvectors() to 'svd' or 'eig'
+    % according to how you want to calculate the eigenvectors
+    eV = getEigenvectors(trainingSet, K(i), 'svd');
     trainProjection = eV' * trainingSet;
     testProjection = eV' * testingSet;
     truePositives = 0;
@@ -64,13 +67,15 @@ for i = 1:size(K, 2)
     recognitionRates(i) = truePositives / M;
 end
 
+figure;
 plot(K, recognitionRates);
 xlabel('K');
 ylabel('Recognition rate');
-title('Recognition rate vs K plot');
+title('Recognition rate vs K');
 
-%% Yale Database testing
+%% Yale dataset testing
 
+numPeople = 
 
 %%
 toc;
