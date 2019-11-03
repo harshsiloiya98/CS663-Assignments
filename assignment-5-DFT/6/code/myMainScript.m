ced6a6707b65b6dafa2ac41010e5416e137c1141
Bhavesh Dhingra <bhavesh@excelsa.local>
2019-11-03 13:58:51 +0530

    A5-Q1,2,6

--- assignment-5-DFT/6/code/myMainScript.m
+++ assignment-5-DFT/6/code/myMainScript.m
@@ -1,6 +1,38 @@
 %% MyMainScript
 
+clc;
+clear;
 tic;
 %% Your code here
+
+% I and J
+I = zeros(300,300);
+I(50:100,50:120) = 255;
+J = zeros(300,300);
+J(120:170,20:90) = 255;
+
+FI = fft2(I);
+FJ = fft2(J);
+Output = (FI .* conj(FJ)) ./ abs(FI.*FJ);
+Inv = ifft2(Output);
+[r,c] = find(Inv == max(max(Inv)));
+
+
+% Adding noise to I and J
+I2 = I + randn(size(I))*20;
+J2 = J + randn(size(J))*20;
+
+FI2 = fft2(I2);
+FJ2 = fft2(J2);
+Output2 = (FI2 .* conj(FJ2)) ./ abs(FI2.*FJ2);
+Inv1 = ifft2(Output2);
+[r1,c1] = find(Inv1 == max(max(Inv1)));
+
+% Displaying logs
+figure(1);
+imagesc(log(1+abs(Output)));
+figure(2);
+imagesc(log(1+abs(Output2)));
+
 
 toc;
